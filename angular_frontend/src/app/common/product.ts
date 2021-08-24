8454ef48eb710e2f59d791e97e944330e88fe949
Boxuan Hu <hu.box@northeastern.edu>
2021-08-23 21:46:30 -0700

    initial integration done

---
+++ angular_frontend/src/app/common/product.ts
@@ -0,0 +1,16 @@
+export class Product {
+
+    sku : string;
+    name : string;
+    decription : string;
+    unitPrice : number;
+    imageUrl : string;
+    active : boolean;
+    unitsInStock : number;
+    dateCreated : Date;
+    lastUpdated : Date;
+
+    
+
+
+}
