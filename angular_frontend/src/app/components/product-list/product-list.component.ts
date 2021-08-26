cc933c188a34ba6973d9fa4bf26b240c528b706f
Boxuan Hu <hu.box@northeastern.edu>
2021-08-26 15:55:58 -0700

    basic search by category done

--- angular_frontend/src/app/components/product-list/product-list.component.ts
+++ angular_frontend/src/app/components/product-list/product-list.component.ts
@@ -1,25 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list-table.component.html',
+  templateUrl: './product-list-grid.component.html',
   styleUrls: ['./product-list.component.css']
 })
 
 export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
+  currentCategoryId: number;
 
-  constructor(private ProductService : ProductService) { }
+  constructor(private ProductService : ProductService, 
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    })
   }
 
   listProducts() {
-    this.ProductService.getProductList().subscribe(
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+    if (hasCategoryId) {
+      // get the id para string. convert string to a numer using the + symbol
+      this.currentCategoryId = Number(this.route.snapshot.paramMap.get('id'));
+    } else {
+      // default id is 1
+      this.currentCategoryId = 1
+    }
+
+    this.ProductService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
