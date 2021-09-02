d5f18d53ae6aa8821d784c7a57080f30b13d1bea
Boxuan Hu <hu.box@northeastern.edu>
2021-09-02 00:13:29 -0700

    fontend secure done

--- java_backend/src/main/java/com/luv2code/ecommerce/config/SecurityConfiguration.java
+++ java_backend/src/main/java/com/luv2code/ecommerce/config/SecurityConfiguration.java
@@ -23,5 +23,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         // force a non-empty response body for 401's to make the response more friendly
         Okta.configureResourceServer401ResponseBody(http);
+
+        // disable csrf since no cookies are used for session tracking
+        http.csrf().disable();
     }
 }
